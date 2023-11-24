0a9ce1755429ced6f3c266d6f1837dda05fcfae4
YD <105weapon@gmail.com>
2023-11-24 19:31:42 +0900

    feat: added isInStock Method In Inventory Service

--- inventory-service/src/main/java/com/ydong/inventoryservice/repository/InventoryRepository.java
+++ inventory-service/src/main/java/com/ydong/inventoryservice/repository/InventoryRepository.java
@@ -1,7 +1,9 @@
 package com.ydong.inventoryservice.repository;
 
 import com.ydong.inventoryservice.model.Inventory;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface InventoryRepository extends JpaRepository<Inventory, Long> {
+    Optional<Inventory> findBySkucode();
 }
---
+++ inventory-service/src/main/java/com/ydong/inventoryservice/service/InventoryService.java
@@ -0,0 +1,19 @@
+package com.ydong.inventoryservice.service;
+
+import com.ydong.inventoryservice.repository.InventoryRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class InventoryService {
+
+    private final InventoryRepository inventoryRepository;
+
+    @Transactional(readOnly = true)
+    public boolean isInStock(String skuCode){
+        return inventoryRepository.findBySkucode().isPresent();
+    }
+
+}
