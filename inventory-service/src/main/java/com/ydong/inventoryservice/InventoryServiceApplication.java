0f23b6f6a60508e92b498cc0f230f789acacb16f
YD <105weapon@gmail.com>
2023-11-24 23:04:06 +0900

    fix(ServiceDiscovery): solved version problem using eureka server, client

--- inventory-service/src/main/java/com/ydong/inventoryservice/InventoryServiceApplication.java
+++ inventory-service/src/main/java/com/ydong/inventoryservice/InventoryServiceApplication.java
@@ -5,11 +5,11 @@ import com.ydong.inventoryservice.repository.InventoryRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-@EnableEurekaClient
+@EnableDiscoveryClient
 public class InventoryServiceApplication {
 
 	public static void main(String[] args) {
