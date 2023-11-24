0a013cec498cfc40b86a533286799a05764e7cd7
YD <105weapon@gmail.com>
2023-11-24 19:33:43 +0900

    feat: added Transactional in order-service

--- order-service/src/main/java/com/ydong/orderservice/service/OrderService.java
+++ order-service/src/main/java/com/ydong/orderservice/service/OrderService.java
@@ -9,9 +9,11 @@ import java.util.List;
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
+@Transactional
 public class OrderService {
 
     private final OrderRepository orderRepository;
