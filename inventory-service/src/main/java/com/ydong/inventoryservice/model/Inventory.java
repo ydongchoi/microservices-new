c66d3894fc51ade6677aff12ac26e5055246d501
YD <105weapon@gmail.com>
2023-11-24 18:53:29 +0900

    feat: added inventory model

---
+++ inventory-service/src/main/java/com/ydong/inventoryservice/model/Inventory.java
@@ -0,0 +1,23 @@
+package com.ydong.inventoryservice.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "t_inventory")
+public class Inventory {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String skuCode;
+    private Integer quantity;
+}
